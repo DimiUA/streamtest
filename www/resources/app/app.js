a1f24bf26dff62a61ff77f27aa675b384bebfbe6
DimiUA <s.dimi.d@gmil.com>
2019-07-07 18:19:42 +0300

    sdsdsd

--- www/resources/app/app.js
+++ www/resources/app/app.js
@@ -33,33 +33,52 @@ var mainView = app.views.create('.view-main', {
 
 
 $$('.buttonStart').on('click', function(){
-	var constraints = navigator.mediaDevices.getSupportedConstraints();
+
+	   var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: source, // 0:Photo Library, 1=Camera, 2=Saved Album
+            encodingType: 0 // 0=JPG 1=PNG
+        };
+
+        if (!navigator) {
+        	alert('navigator not found');
+        	return;
+        }
+        if (!navigator.camera) {
+        	alert('camera not found');
+        	return;
+        }
+        navigator.camera.getPicture(
+            function(imgData) {
+            	alert(imgData);
+                //$('.media-object', this.$el).attr('src', "data:image/jpeg;base64,"+imgData);
+                /*mainView.router.load({
+                    url: 'resources/templates/asset.edit.photo.html',
+                    context: {
+                        imgSrc: "data:image/jpeg;base64," + imgData
+                    }
+                });*/
+
+            },
+            function() {
+                alert('Error taking picture');
+            },
+            options);
+
+	/*var constraints = navigator.mediaDevices.getSupportedConstraints();
 	console.log(constraints);
 
-	navigator.mediaDevices.getUserMedia({
-		  'video': {
-		  	'audio': true,
-		    'facingMode': 'environment'
-		  }
+		navigator.mediaDevices.getUserMedia({
+			'video': {
+			  	'audio': true,
+			    'facingMode': 'environment'
+			}
 		}).then(function(mediaStream) {
-		  var mediaControl = document.querySelector('video');
-		  mediaControl.srcObject = mediaStream;
-		  mediaControl.src = URL.createObjectURL(mediaStream);
-		});
+			var mediaControl = document.querySelector('video');
+			mediaControl.srcObject = mediaStream;
+			mediaControl.src = URL.createObjectURL(mediaStream);
+		});*/
 
-	/*navigator.mediaDevices.getUserMedia({
-	   
-	    'video': {
-	        facingMode: 'user'
-	    }
-	}).then(function(mediaStream) {
-	  // do something with the media stream
-	  	$$('.videoContainer').html(`
-			<video width="320" height="240" controls>
-			  <source src="movie.mp4" type="video/mp4">
-			  <source src="movie.ogg" type="video/ogg">
-			  Your browser does not support the video tag.
-			</video>
-	  		`);
-	});*/
+	
 });
